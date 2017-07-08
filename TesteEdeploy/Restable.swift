ca2b5fc8984cfb8236263049c58eefae2f724392
Diego Cavalcante <diegocavalcante@MacBookvalcante.home>
2017-07-07 22:10:54 -0300

    Versão finalizada para análise

--- TesteEdeploy/Restable.swift
+++ TesteEdeploy/Restable.swift
@@ -44,11 +44,7 @@ extension Restable {
         
         var request = URLRequest(url: url)
         request.httpMethod  = HttpMethod.Post.rawValue
-        request.httpBody    = try? JSONSerialization.data(withJSONObject: params ?? [:], options: .prettyPrinted)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "content-type")
-        
+        request.httpBody    = try? JSONSerialization.data(withJSONObject: params ?? [:], options: [])
         
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
